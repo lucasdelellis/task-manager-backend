275fb71d80e5fdcde5026025dfe90d9639d5f8d1
Lucas <lucasdelellis2002@gmail.com>
2022-07-04 16:26:20 -0300

    add: attempAuthentication in CustomAuthenticationFilter

---
+++ src/main/java/com/lucazz82/task/filters/CustomAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.lucazz82.task.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+	
+	public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+		super();
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		String username = request.getParameter(SPRING_SECURITY_FORM_USERNAME_KEY);
+		String password = request.getParameter(SPRING_SECURITY_FORM_PASSWORD_KEY);
+		
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		
+		return this.authenticationManager.authenticate(authenticationToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+	}
+	
+}
