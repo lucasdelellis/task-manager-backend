ba30e41a8349894dfe988480a00532f4f0afaa0d
Lucas <lucasdelellis2002@gmail.com>
2022-07-04 17:32:43 -0300

    add: protection for all urls except register and login.

--- src/main/java/com/lucazz82/task/security/SecurityConfig.java
+++ src/main/java/com/lucazz82/task/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.lucazz82.task.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll(); // Register
+		http.authorizeRequests().anyRequest().authenticated();
 		http.addFilter(new CustomAuthenticationFilter(this.authenticationManagerBean()));
 	}
 
