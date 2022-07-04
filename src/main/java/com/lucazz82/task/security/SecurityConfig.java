79be020e51ed4813369ef59da3605da01afb60e1
Lucas <lucasdelellis2002@gmail.com>
2022-07-04 16:26:45 -0300

    add: configuration for http security

--- src/main/java/com/lucazz82/task/security/SecurityConfig.java
+++ src/main/java/com/lucazz82/task/security/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.lucazz82.task.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.lucazz82.task.filters.CustomAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthenticationFilter(this.authenticationManagerBean()));
 	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	
 
 }
