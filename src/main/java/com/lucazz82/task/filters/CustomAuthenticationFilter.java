ffad09e836e4a458caf223990bea35870ff585d6
Lucas <lucasdelellis2002@gmail.com>
2022-07-04 16:42:18 -0300

    add: successfulAuthentication

--- src/main/java/com/lucazz82/task/filters/CustomAuthenticationFilter.java
+++ src/main/java/com/lucazz82/task/filters/CustomAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.lucazz82.task.filters;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,12 +13,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
 	private AuthenticationManager authenticationManager;
-	
+
 	public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
 		super();
 		this.authenticationManager = authenticationManager;
@@ -27,16 +33,32 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 			throws AuthenticationException {
 		String username = request.getParameter(SPRING_SECURITY_FORM_USERNAME_KEY);
 		String password = request.getParameter(SPRING_SECURITY_FORM_PASSWORD_KEY);
-		
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-		
+
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+				password);
+
 		return this.authenticationManager.authenticate(authenticationToken);
 	}
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-			Authentication authResult) throws IOException, ServletException {
+			Authentication authentication) throws IOException, ServletException {
+		User user = (User) authentication.getPrincipal();
+		Algorithm algorithm = Algorithm.HMAC256("secret key".getBytes());
+
+		String access_token = JWT.create().withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+				.withIssuer(request.getRequestURL().toString())
+				.withClaim("roles", new ArrayList<>()) // List of roles. Not implemented yet
+				.sign(algorithm);
 		
+		String refresh_token = JWT.create().withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+				.withIssuer(request.getRequestURL().toString())
+				.sign(algorithm);
+
+		response.setHeader("access_token", access_token);
+		response.setHeader("refresh_token", refresh_token);
 	}
-	
+
 }
