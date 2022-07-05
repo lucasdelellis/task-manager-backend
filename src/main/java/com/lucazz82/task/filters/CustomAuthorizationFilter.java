a772861d2aabf404663fbc30d5fe8c221809bde0
Lucas <lucasdelellis2002@gmail.com>
2022-07-05 10:41:04 -0300

    add: catch exceptions from filters.

--- src/main/java/com/lucazz82/task/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/lucazz82/task/filters/CustomAuthorizationFilter.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,39 +31,42 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		String path = request.getServletPath();
+		String method = request.getMethod();
+		
+		if(isProtected(path, method)) {
+			String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+			if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				String token = authorizationHeader.substring("Bearer ".length());
+				Algorithm algorithm = Algorithm.HMAC256("secret key".getBytes());
+				JWTVerifier verifier = JWT.require(algorithm).build();
+				DecodedJWT decodedJWT = verifier.verify(token);
+				String username = decodedJWT.getSubject(); // Obtain username
+				// Here we can collect roles if correspond
+				UsernamePasswordAuthenticationToken authenticationToken = 
+						new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
+				SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+				filterChain.doFilter(request, response);				
+			} else {
+				throw new ServletException("Invalid token");
+			}
+		}
+		
+		filterChain.doFilter(request, response);
+
+	}
+	
+	private boolean isProtected(String path, String method) {
+		boolean response = true;
 		
 		if(path.equals("/login")) {
-			filterChain.doFilter(request, response);
-		} else if(path.equals("/user") && request.getMethod().equals("POST")) {
-			filterChain.doFilter(request, response);
-		} else {
-//			try {
-				String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-				if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-					String token = authorizationHeader.substring("Bearer ".length());
-					Algorithm algorithm = Algorithm.HMAC256("secret key".getBytes());
-					JWTVerifier verifier = JWT.require(algorithm).build();
-					DecodedJWT decodedJWT = verifier.verify(token);
-					String username = decodedJWT.getSubject(); // Obtain username
-					// Here we can collect roles if correspond
-					UsernamePasswordAuthenticationToken authenticationToken = 
-							new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
-					SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-					filterChain.doFilter(request, response);				
-				} else {
-					filterChain.doFilter(request, response);
-				}
-//			} catch (Exception e) {
-//				Map<String, String> error = new HashMap<>();
-//				error.put("message", e.getMessage());
-//				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//				ObjectMapper objectMapper = new ObjectMapper();
-//				objectMapper.writeValue(response.getOutputStream(), error);
-//				response.setStatus(HttpStatus.FORBIDDEN.value());
-//			}
-
+			response = false;
+		} 
+		
+		if(path.equals("/user") && method.equals(HttpMethod.POST.name())) {
+			response = false;
 		}
-
+		
+		return response;
 	}
 
 }
