54e844292b7efd6d944ae33563e2a8bf5c32773c
Lucas <lucasdelellis2002@gmail.com>
2022-07-15 12:18:25 -0300

    fix: new non protected endpoints.

--- src/main/java/com/lucazz82/task/security/SecurityConfig.java
+++ src/main/java/com/lucazz82/task/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll(); // Register
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/auth/register").permitAll(); // Register
 		http.authorizeRequests().anyRequest().authenticated();
 		http.addFilter(new CustomAuthenticationFilter(this.authenticationManagerBean()));
 		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
