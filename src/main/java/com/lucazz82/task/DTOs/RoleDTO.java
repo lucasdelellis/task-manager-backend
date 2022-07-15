887f7b6c3f2b01f19a4be08574d8bba0c8c2fc9f
Lucas <lucasdelellis2002@gmail.com>
2022-07-15 11:18:12 -0300

    feat: simple task dto.

---
+++ src/main/java/com/lucazz82/task/DTOs/RoleDTO.java
@@ -0,0 +1,8 @@
+package com.lucazz82.task.DTOs;
+
+import lombok.Data;
+
+@Data
+public class RoleDTO {
+	private String name;
+}
---
+++ src/main/java/com/lucazz82/task/DTOs/SimpleTaskDTO.java
@@ -0,0 +1,10 @@
+package com.lucazz82.task.DTOs;
+
+import lombok.Data;
+
+@Data
+public class SimpleTaskDTO {
+	private Long id;
+	private String content;
+}
+
