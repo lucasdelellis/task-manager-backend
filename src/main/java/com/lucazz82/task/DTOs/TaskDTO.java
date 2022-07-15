dbe8cda75c012fe129029e4e2f8e6cdfe10be049
Lucas <lucasdelellis2002@gmail.com>
2022-07-15 11:17:51 -0300

    add: dto with lombok.

--- src/main/java/com/lucazz82/task/DTOs/TaskDTO.java
+++ src/main/java/com/lucazz82/task/DTOs/TaskDTO.java
@@ -1,42 +1,10 @@
 package com.lucazz82.task.DTOs;
 
+import lombok.Data;
+
+@Data
 public class TaskDTO {
 	private Long id;
 	private String content;
-	private UserDTO user;
-	
-	public TaskDTO() {
-	}
-	
-	public TaskDTO(Long id, String content, UserDTO user) {
-		this.id = id;
-		this.content = content;
-		this.user = user;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public UserDTO getUser() {
-		return user;
-	}
-
-	public void setUser(UserDTO user) {
-		this.user = user;
-	}
-	
-	
+	private UserDTO user;	
 }
--- src/main/java/com/lucazz82/task/DTOs/UserDTO.java
+++ src/main/java/com/lucazz82/task/DTOs/UserDTO.java
@@ -1,48 +1,12 @@
 package com.lucazz82.task.DTOs;
 
-import java.util.List;
+import java.util.Set;
 
-import com.lucazz82.task.models.TaskModel;
+import lombok.Data;
 
+@Data
 public class UserDTO {
 	private Long id;
 	private String username;
-	private List<TaskModel> tasks;
-	
-	public UserDTO() {
-		super();
-	}
-
-	public UserDTO(Long id, String username, List<TaskModel> tasks) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.tasks = tasks;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public List<TaskModel> getTasks() {
-		return tasks;
-	}
-
-	public void setTasks(List<TaskModel> tasks) {
-		this.tasks = tasks;
-	}
-	
-	
+	private Set<RoleDTO> roles;
 }
