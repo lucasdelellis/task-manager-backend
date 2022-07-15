bf2f2b6c54e3f8d8214cf98bebc6faf0a04666c7
Lucas <lucasdelellis2002@gmail.com>
2022-07-15 11:19:24 -0300

    feat: role repository.

---
+++ src/main/java/com/lucazz82/task/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.lucazz82.task.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.lucazz82.task.enums.Roles;
+import com.lucazz82.task.models.RoleModel;
+
+public interface RoleRepository extends CrudRepository<RoleModel, Long> {
+	public Optional<RoleModel> findByRole(Roles roles);
+}
