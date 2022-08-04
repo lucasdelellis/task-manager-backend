d8d261313bc459dd6d75b196c4295a2507e0321f
Lucas <lucasdelellis2002@gmail.com>
2022-08-04 10:38:36 -0300

    feat: endpoints using dtos and validated users.

--- src/main/java/com/lucazz82/task/controllers/TaskController.java
+++ src/main/java/com/lucazz82/task/controllers/TaskController.java
@@ -1,14 +1,13 @@
 package com.lucazz82.task.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,51 +17,122 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucazz82.task.DTOs.SimpleTaskDTO;
 import com.lucazz82.task.DTOs.TaskDTO;
 import com.lucazz82.task.DTOs.UtilDTO;
+import com.lucazz82.task.handlers.ForbiddenException;
 import com.lucazz82.task.models.TaskModel;
+import com.lucazz82.task.models.UserModel;
 import com.lucazz82.task.services.TaskService;
+import com.lucazz82.task.services.UserService;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/user/{user_id}/task")
 public class TaskController {
 	@Autowired
 	private TaskService _taskService;
 	
 	@Autowired
-	private UtilDTO utilDTO;
+	private UserService _userService;	
 	
-	
+	/**
+	 * Get all tasks for a given user.
+	 * @param userId id of the user.
+	 * @return all tasks.
+	 */
 	@GetMapping()
-	public ResponseEntity<ArrayList<TaskDTO>> getTasks() {
-		ArrayList<TaskModel> tasks = _taskService.getTasks();
-		ArrayList<TaskDTO> tasksDTO = new ArrayList<>(); 
+	public ResponseEntity<ArrayList<SimpleTaskDTO>> getTasks(@PathVariable("user_id") Long userId) {
+		UserModel user = getValidatedUser(userId);
+		
+		List<TaskModel> tasks = user.getTasks();
+		
+		ArrayList<SimpleTaskDTO> tasksDTO = new ArrayList<>(); 
+		
 		tasks.forEach(task -> {
-			tasksDTO.add(utilDTO.taskDTOfromTask(task));
+			tasksDTO.add(UtilDTO.simpleTaskFromTaskModel(task));
 		});
 		
 		return new ResponseEntity<>(tasksDTO, HttpStatus.OK);
 	}
 	
+	/**
+	 * Return task of the given id.
+	 * @param userId id of the user.
+	 * @param id id of the task.
+	 * @return task.
+	 */
 	@GetMapping(path = "/{id}")
-	public ResponseEntity<TaskDTO> getTask(@PathVariable("id") Long id) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String username = (String) authentication.getPrincipal();
-		return new ResponseEntity<>(utilDTO.taskDTOfromTask(_taskService.getTask(id)), HttpStatus.OK);
+	public ResponseEntity<TaskDTO> getTask(@PathVariable("user_id") Long userId, @PathVariable("id") Long id) {
+		UserModel user = getValidatedUser(userId);
+		
+		TaskModel task = _taskService.getTask(user, id);
+		TaskDTO dto = UtilDTO.taskDTOFromTaskModel(task);
+		
+		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 	
+	
+	/**
+	 * Create new task.
+	 * @param userId
+	 * @param taskDTO
+	 * @return
+	 */
 	@PostMapping()
-	public ResponseEntity<TaskDTO> newTask(@Valid @RequestBody TaskModel task) {
-		return new ResponseEntity<>(utilDTO.taskDTOfromTask(_taskService.newTask(task)), HttpStatus.CREATED);
+	public ResponseEntity<TaskDTO> newTask(@PathVariable("user_id") Long userId, @Valid @RequestBody TaskDTO taskDTO) {
+		UserModel user = getValidatedUser(userId);
+		
+		TaskModel task = UtilDTO.taskModelFromTaskDTO(taskDTO);
+		
+		return new ResponseEntity<>(UtilDTO.taskDTOFromTaskModel(_taskService.newTask(user, task)), HttpStatus.CREATED);
 	}
-	
+
+	/**
+	 * Delete task by id. 
+	 * @param userId
+	 * @param id
+	 * @return
+	 */
 	@DeleteMapping(path = "/{id}")
-	public ResponseEntity<TaskDTO> deleteTask(@PathVariable Long id) {
-		return new ResponseEntity<>(utilDTO.taskDTOfromTask(_taskService.deleteTask(id)), HttpStatus.OK);
+	public ResponseEntity<TaskDTO> deleteTask(@PathVariable("user_id") Long userId, @PathVariable Long id) {
+		UserModel user = getValidatedUser(userId);
+		
+		TaskModel task = _taskService.deleteTask(user, id);
+		TaskDTO dto = UtilDTO.taskDTOFromTaskModel(task);
+		
+		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
 	
+	/**
+	 * Edit task by id. 
+	 * @param userId
+	 * @param id
+	 * @param newTask
+	 * @return
+	 */
 	@PutMapping(path = "/{id}")
-	public ResponseEntity<TaskDTO> editTask(@PathVariable Long id,@Valid @RequestBody TaskModel task) {
-		return new ResponseEntity<>(utilDTO.taskDTOfromTask(_taskService.editTask(id, task)), HttpStatus.OK);
+	public ResponseEntity<TaskDTO> editTask(@PathVariable("user_id") Long userId, @PathVariable Long id, @Valid @RequestBody TaskDTO newTask) {
+		UserModel user = getValidatedUser(userId);
+		
+		TaskModel task = UtilDTO.taskModelFromTaskDTO(newTask);
+		task = _taskService.editTask(user, id, task);
+		TaskDTO dto = UtilDTO.taskDTOFromTaskModel(task);
+		
+		return new ResponseEntity<>(dto, HttpStatus.OK);
+	}
+
+	/**
+	 * Validate that the user in security context match the id given in path. 
+	 * @param userId
+	 * @return
+	 */
+	private UserModel getValidatedUser(Long userId) {
+		UserModel user = _userService.getUserFromSecurityContext();
+		
+		if(user.getId() != userId) {
+			throw new ForbiddenException("invalid user id");
+		}
+		
+		return user;
 	}
 }
--- src/main/java/com/lucazz82/task/services/TaskService.java
+++ src/main/java/com/lucazz82/task/services/TaskService.java
@@ -1,60 +1,49 @@
 package com.lucazz82.task.services;
 
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
-import com.lucazz82.task.handlers.BadRequestException;
+import com.lucazz82.task.handlers.ForbiddenException;
 import com.lucazz82.task.handlers.NotFoundException;
-import com.lucazz82.task.handlers.ServerErrorException;
 import com.lucazz82.task.models.TaskModel;
 import com.lucazz82.task.models.UserModel;
 import com.lucazz82.task.repositories.TaskRepository;
-import com.lucazz82.task.repositories.UserRepository;
 
 @Service
 public class TaskService {
 	@Autowired
 	private TaskRepository _taskRepository;
-	@Autowired
-	private UserRepository _userRepository;
 	
 	public ArrayList<TaskModel> getTasks() {
 		return _taskRepository.findAll();
 	}
 	
-	public TaskModel getTask(Long id) {
-		try {
-			TaskModel task = _taskRepository.findById(id).orElseThrow(() -> new NotFoundException("Task not found"));
-			return task;
-			
-		} catch (NotFoundException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new ServerErrorException("Internal Server Error");
+	public TaskModel getTask(UserModel user, Long id) {
+		TaskModel task = _taskRepository.findById(id).orElseThrow(() -> new NotFoundException("Task not found"));
+		
+		if(task.getUser() != user) {
+			throw new ForbiddenException("access denied to task with id " + id);
 		}
 		
+		return task;		
 	}
 	
-	public TaskModel newTask(TaskModel task) {
-		UserModel user = _userRepository.findById(task.getUser().getId()).orElseThrow(() -> new NotFoundException("User not found"));
+	public TaskModel newTask(UserModel user, TaskModel task) {
 		task.setUser(user);
 		_taskRepository.save(task);
 		return task;	
 	}
 	
-	public TaskModel deleteTask(Long id) {
-		TaskModel task = getTask(id);
+	public TaskModel deleteTask(UserModel user, Long id) {
+		TaskModel task = getTask(user, id);
 		_taskRepository.delete(task);
 		return task;			
 	}
 	
-	public TaskModel editTask(Long id, TaskModel editedTask) {
-		TaskModel task = getTask(id);
+	public TaskModel editTask(UserModel user, Long id, TaskModel editedTask) {
+		TaskModel task = getTask(user, id);
 		task.setContent(editedTask.getContent());
 		_taskRepository.save(task);
 		return task;	
