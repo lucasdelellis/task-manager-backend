b2f39cf5967596767dc3333892f4ca93ae9d787d
Lucas <lucasdelellis2002@gmail.com>
2022-08-09 12:20:21 -0300

    add: constrains to DTOs.

--- src/main/java/com/lucazz82/task/DTOs/RegisterDTO.java
+++ src/main/java/com/lucazz82/task/DTOs/RegisterDTO.java
@@ -1,9 +1,13 @@
 package com.lucazz82.task.DTOs;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class RegisterDTO {
+	@NotBlank(message = "username cannot be blank")
 	private String username;
+	@NotBlank(message = "password cannot be blank")
 	private String password;
 }
--- src/main/java/com/lucazz82/task/DTOs/TaskDTO.java
+++ src/main/java/com/lucazz82/task/DTOs/TaskDTO.java
@@ -1,10 +1,13 @@
 package com.lucazz82.task.DTOs;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class TaskDTO {
 	private Long id;
+	@NotBlank(message = "Content cannot be blank")
 	private String content;
 	private UserDTO user;	
 }
