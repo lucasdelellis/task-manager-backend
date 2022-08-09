bba3a39746a8b28a3b027ebefcb27ec5368cc268
Lucas <lucasdelellis2002@gmail.com>
2022-08-09 12:19:48 -0300

    fix: change error messages.

--- src/main/java/com/lucazz82/task/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/lucazz82/task/handlers/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,47 +27,57 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ForbiddenException.class)
 	@ResponseBody
 	public ErrorMessage forbidden(ForbiddenException exception) {
-		return new ErrorMessage(200, exception.getMessage());
+		return new ErrorMessage(201, exception.getMessage());
 	}
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(BadRequestException.class)
 	@ResponseBody
 	public ErrorMessage badRequest(BadRequestException exception) {
-		return new ErrorMessage(201, exception.getMessage());
+		return new ErrorMessage(202, exception.getMessage());
 	}
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseBody
 	public ErrorMessage badRequest(MethodArgumentNotValidException exception) {
-		List<Violation> error = new ArrayList<>();
-		for(FieldError e : exception.getBindingResult().getFieldErrors()) {
-			error.add(new Violation(e.getField(), e.getDefaultMessage()));
+		List<Violation> errors = new ArrayList<>();
+		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+		
+		for(FieldError e : fieldErrors) {
+			errors.add(new Violation(e.getField(), e.getDefaultMessage()));
 		}
 		
-		return new ErrorMessage(201, error.toString());
+		return new ErrorMessage(203, errors.toString());
 	}
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	@ResponseBody
 	public ErrorMessage badRequest(DataIntegrityViolationException exception) {
-		return new ErrorMessage(501, "Incorrect data");
+		return new ErrorMessage(204, "Incorrect data");
 	}
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(NotUniqueUsernameException.class)
 	@ResponseBody
 	public ErrorMessage badRequest(NotUniqueUsernameException exception) {
-		return new ErrorMessage(201, exception.getMessage());
+		return new ErrorMessage(205, exception.getMessage());
+	}
+	
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	@ResponseBody
+	public ErrorMessage badRequest(HttpMessageNotReadableException exception) {
+		String message = exception.getMessage().split(":")[0];
+		return new ErrorMessage(206, message);
 	}
 	
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
 	public ErrorMessage ServerError(Exception exception) {
-		exception.printStackTrace();
-		return new ErrorMessage(201, exception.getMessage());
+		return new ErrorMessage(500, "internal server error");
 	}
 }
